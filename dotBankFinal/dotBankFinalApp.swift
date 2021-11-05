5a11866a42550b14ce5dc15072025b4eb52b3228
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-11-05 18:44:36 -0400

    Initial Commit

---
+++ dotBankFinal/dotBankFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotBankFinalApp.swift
+//  dotBankFinal
+//
+//  Created by Tom on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct dotBankFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
