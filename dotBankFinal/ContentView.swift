b5dc75cb94ec556afab307ae1704ff69d76096a1
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2021-11-05 19:29:40 -0400

    Commit Début à L'Hotel Trois Rivières

--- dotBankFinal/ContentView.swift
+++ dotBankFinal/ContentView.swift
@@ -9,13 +9,101 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            HomeView()
+                .tabItem {
+                VStack {
+                    Image(systemName: "creditcard")
+                    Text("Accounts")
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13 Pro Max")
     }
+}
+
+
+// MARK: - EXTENTIONS
+
+// MARK: Color Extention
+extension Color {
+    static func hexToColor (hex: String, opacity: Double = 1.0) -> Color {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        if ((cString.count) != 6) {
+            return Color.gray
+        }
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+        return Color(
+            red: Double((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: Double((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: Double(rgbValue & 0x0000FF) / 255.0,
+            opacity: Double(opacity)
+        )
+    }
+}
+
+// MARK: String Extention
+extension String {
+    func cleanDollars(_ value: String?) -> String {
+        guard value != nil else { return "$0.00" }
+        let doubleValue = Double(value!) ?? 0.0
+        let formatter = NumberFormatter()
+        formatter.currencyCode = "USD"
+        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = (value!.contains(".00")) ? 0 : 2
+        formatter.maximumFractionDigits = 2
+        formatter.numberStyle = .currencyAccounting
+
+        return formatter.string(from: NSNumber(value: doubleValue)) ?? "$\(doubleValue)"
+    }
+
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+
+    func substring(from: Int) -> String {
+        let fromIndex = (from > 0 ? index(from: from) : index(from: self.count - abs(from)))
+        return String(self[fromIndex...])
+    }
+
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return String(self[..<toIndex])
+    }
+
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return String(self[startIndex..<endIndex])
+    }
+}
+
+// MARK: - STRUCTURES
+
+struct Account: Identifiable {
+    var id: Int { number }
+
+    var number: Int
+    var name: String
+    var money: Double
+}
+
+// MARK: - Structure Card
+struct Card: Identifiable {
+    var id: Int { number }
+
+    var number: Int
+    var name: String
+    var money: Double
 }
---
+++ dotBankFinal/HomeView.swift
@@ -0,0 +1,60 @@
+//
+//  HomeView.swift
+//  dotBankFinal
+//
+//  Created by Tom on 05/11/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    // MARK: - VARIABLES
+
+        // MARK: Colors
+    let backgroundColor: Color = Color.hexToColor(hex: "#1C1C1E")
+    let greyTextColor: Color = Color.hexToColor(hex: "#98989F")
+    let blueTextColor: Color = Color.hexToColor(hex: "#0A84FF")
+
+        // MARK: Accounts
+    let account1 = Account(number: 1322213451234, name: "Total Checking", money: 14234.32)
+    let account2 = Account(number: 423521345, name: "Buisness Checking", money: 2134.69)
+
+        // MARK: Cards
+    let card1 = Card(number: 4724332197467784, name: "Platinum Mastercard", money: 241.65)
+    let card2 = Card(number: 7413686531874906, name: "Cash Reward Visa", money: 1576.03)
+
+    var body: some View {
+        ZStack {
+            backgroundColor
+                .ignoresSafeArea()
+            VStack {
+                VStack {
+                    HStack {
+                        Image(systemName: "text.bubble")
+                        Spacer()
+                        Image(systemName: "bell")
+                    }
+                    .font(.system(size: 25))
+                    .foregroundColor(blueTextColor)
+                    .padding()
+
+                    HStack {
+                        Text("Accounts")
+                            .font(.system(size: 40, weight: .semibold, design: .rounded))
+                        Spacer()
+                    }.padding(.leading)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13 Pro Max")
+    }
+}
